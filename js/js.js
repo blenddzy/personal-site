734a2616bba8d82096cae95ee4430f430a74a766
Federico Gregori <58458968+blenddzy@users.noreply.github.com>
2021-06-11 18:21:54 -0300

    frist commit

---
+++ js/js.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    var menuToggle = documen.querySelector('.toggle');
+    var menu = documen.querySelector('.menu');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
